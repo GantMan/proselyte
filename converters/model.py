d51d08f2819a0d6d159a94319031ffcdaf22337d
Selvam <35126919+selvam33@users.noreply.github.com>
2019-10-03 17:00:16 +0530

    Update model.py

--- converters/model.py
+++ converters/model.py
@@ -7,11 +7,9 @@ import os
 
 
 keras_model = './path/to/load/the/keras/model/file.h5' # path of the Keras model file
-tfjs_dir = '.path/to/save/the/tensorflowjs_file' # path of the Tensorflow JS directory
-tflite_dir = './path/to/tensorflowjs_file' # path of the Tensorflow LITE directory
-tflite_file = '/model.tflite'
-tf_dir = '.path/to/save/the/tensorflow_file' # path of the Tensorflow directory
-tf_file = '/model.pb' # Tensorflow file name
+path_of_tfjs_dir = '.path/to/save/the/tensorflowjs_file' # path of the Tensorflow JS directory
+path_of_tflite_dir = './path/to/tensorflowjs_file' # path of the Tensorflow LITE directory
+path_of_tf_dir = '.path/to/save/the/tensorflow_file' # path of the Tensorflow directory
 
 zip_dir = './path/to.zip/all/the/models' #path of the zip directiory
 
@@ -34,34 +32,38 @@ def freeze_session(session, keep_var_names=None, output_names=None, clear_device
             session, input_graph_def, output_names, freeze_var_names)
         return frozen_graph
       
+def keras2tf(path_of_keras_file, path_of_tf_dir, file='\file_name.pb')
+    model = load_model(path_of_keras_file)
+    frozen_graph = freeze_session(K.get_session(), output_names=[out.op.name for out in model.outputs])
+    tf_pb = tf.train.write_graph(frozen_graph, path, file, as_text=False)
+    return (tf_pb)
+    
 
-frozen_graph = freeze_session(K.get_session(), output_names=[out.op.name for out in model.outputs])
-tf_pb = tf.train.write_graph(frozen_graph, tf_dir, tf_file, as_text=False)
+def keras2tflite(path_of_keras_file, path_of_tflite_dir, tflite_file_name = '\file_name.tflite'):
+    # Convert Keras to TFLITE
+    converter = tf.lite.TFLiteConverter.from_keras_model_file(path_of_keras_file)
+    tfmodel = converter.convert()
+    tflite = open(path_of_tflite_dir + tflite_file_name,"wb").write(tfmodel)
+    return tflite
 
-
-# Convert Keras to TFLITE
-converter = tf.lite.TFLiteConverter.from_keras_model_file(keras_model)
-tfmodel = converter.convert()
-tflite = open(tflite_dir + tflite_file,"wb").write(tfmodel)
-
-# Convert Keras to Tensorflow JS
-# If tensorflow JS library missing the dispatch_keras_h5_to_tfjs_layers_model_conversion function in init.py add the function
-tf_js = converters.dispatch_keras_h5_to_tfjs_layers_model_conversion(keras_model, tfjs_dir)
+def keras2tfjs(path_of_keras_file, path_of_tfjs_dir):
+    # Convert Keras to Tensorflow JS
+    # If tensorflow JS library missing the dispatch_keras_h5_to_tfjs_layers_model_conversion function in init.py add the function
+    tf_js = converters.dispatch_keras_h5_to_tfjs_layers_model_conversion(path_of_keras_file, path_of_tfjs_dir)
+    return tf_js
 
 # Zip all the Files
 zipObj = ZipFile(zip_dir + r'file.zip', 'w')
  
 # Add multiple files to the zip
-zipObj.write(tflite_dir + r'\file.tflite')
-zipObj.write(tf_dir + r'\file.pb')
-zipObj.write(tfjs_dir + r'\file.json')
+zipObj.write(path_of_tflite_dir + r'\file.tflite')
+zipObj.write(path_of_tf_dir + r'\file.pb')
+zipObj.write(path_of_tfjs_dir + r'\file.json')
  
 # Close the Zip File
 zipObj.close()
 
 # Removing the files after Zip
-os.remove(tflite_dir + r'\file.tflite')
-os.remove(tf_dir + r'\file.pb')
-os.remove(tfjs_dir + r'\file.json')
-
-
+os.remove(path_of_tflite_dir + r'\file.tflite')
+os.remove(path_of_tf_dir + r'\file.pb')
+os.remove(path_of_tfjs_dir + r'\file.json')
